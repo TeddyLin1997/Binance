6a75e7bfb7ce341dd3d694de8c128d3bd8c5ac86
ChrisLin <i100308326@gmail.com>
2021-02-28 13:50:53 +0800

    feat:fast function

--- src/pages/views/home/index.tsx
+++ src/pages/views/home/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import banner1 from '../../../images/banner1.jpg'
-// import go from '../../../images/go.svg'
+import go from '../../../images/go.svg'
 import arrow from '../../../images/arrow.svg'
+import global from '../../../images/global.svg'
+import wallet from '../../../images/wallet.svg'
 
 const hotList = [
 	{ label: 'Bitcoin', price: 51179, change: 100, percent: '1.59%' },
@@ -39,11 +41,18 @@ const Home: React.FC = () => {
 					<span>快捷功能</span>
 				</div>
 				<div className="flex justify-between font-bold">
-					<div className="mr-2 py-1 px-2 w-1/2 h-20 text-blue bg-active rounded-lg">
-						<div>電子錢包</div>
-						<img src="" />
+					<div className="relative mr-2 py-1 px-2 w-1/2 h-18 bg-active rounded-lg">
+						<div className="mb-2 text-blue">電子錢包</div>
+						<span className="text-sm">前往</span>
+						<img className="inline-block h-3" src={ go } />
+						<img className="absolute bottom-2 right-4 h-10 transform -rotate-12" src={wallet} />
 					</div>
-					<div className="ml-2 py-1 px-2 w-1/2 h-20 bg-secondary rounded-lg">資產管理</div>
+					<div className="relative ml-2 py-1 px-2 w-1/2 h-18 bg-secondary rounded-lg">
+						<div className="mb-2">資產管理</div>
+						<span className="text-sm">前往</span>
+						<img className="inline-block h-3" src={ go } />
+						<img className="absolute bottom-2 right-4 h-10 transform -rotate-12" src={global} />
+					</div>
 				</div>
 
 				<div className="my-2 flex items-center">
