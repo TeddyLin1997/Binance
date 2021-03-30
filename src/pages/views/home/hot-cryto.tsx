ae74f59c0a116ac84b386495d6deae51801e7a58
ChrisLin1997 <i100308326@gmail.com>
2021-03-30 15:15:58 +0800

    fix:hot crypto style

--- src/pages/views/home/hot-cryto.tsx
+++ src/pages/views/home/hot-cryto.tsx
@@ -9,15 +9,15 @@ const hotCrypto: React.FC = () => {
 	const hotCryptoNode = hotList.map(item => {
 		const color = item.change >= 0 ? 'text-green' : 'text-red'
 		return (
-			<article className="px-2 py-1 w-28 h-20 flex flex-col justify-between bg-white rounded-lg font-bold text-center text-sm" key={item.label}>
+			<article className="px-2 py-1 w-28 h-20 flex flex-col justify-between bg-white rounded-lg font-bold text-center text-xs" key={item.label}>
 				<span className=" text-secondary">{ item.label }</span>
 				<div className="flex justify-between items-end">
 					<span className="text-gray transform scale-90">價格</span>
-					<span className={ color }>{ item.price }</span>
+					<span className={ color + ' text-sm' }>{ item.price }</span>
 				</div>
 				<div className="flex justify-between items-end">
 					<span className="text-gray transform scale-90">漲跌</span>
-					<span className={ color }>{ item.changePercent }</span>
+					<span className={ color + ' text-sm' }>{ item.changePercent }</span>
 				</div>
 			</article>
 		)
