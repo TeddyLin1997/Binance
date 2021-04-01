f4de5bb6ce68851b791c2cfa7774bd80dda06e2a
ChrisLin <i100308326@gmail.com>
2021-04-01 12:15:06 +0800

    fix:quote list rerender

--- src/pages/views/trade/index.tsx
+++ src/pages/views/trade/index.tsx
@@ -10,6 +10,41 @@ type product = {
 
 const getRandom = (range: number): number => Math.floor(Math.random() * (range) + 1)
 
+// button
+const TradeButton: React.FC<{ sign: string }> = ({ sign }) => {
+	return (<span className="text-3xl"> { sign } </span>)
+}
+
+// quote
+const TradeQuote: React.FC<{ price: number }> = React.memo(({ price }) => {
+	const sellQuoteList = Array.from(Array(5).keys()).map(item => {
+		return (
+			<div className="flex justify-between" key={ item }>
+				<span> { getRandom(100) } </span>
+				<span> { price + (item + 1) } </span>
+			</div>
+		)
+	})
+	const buyQuoteList = Array.from(Array(5).keys()).map(item => {
+		return (
+			<div className="flex justify-between" key={ item }>
+				<span> { getRandom(100) } </span>
+				<span> { price - (item + 1) } </span>
+			</div>
+		)
+	})
+
+	return (
+		<div className="text-center">
+			<h4 className="my-2">最佳五檔</h4>
+			<div className="mx-4 w-2/5 inline-flex flex-col text-green"> { buyQuoteList } </div>
+			<div className="mx-4 w-2/5 inline-flex flex-col text-red"> { sellQuoteList } </div>
+		</div>
+	)
+})
+TradeQuote.displayName = 'TradeQuote'
+
+// trade
 const Trade: React.FC = () => {
 	const location: { state: string } = useLocation()
 	const [ product, setProduct ] = useState<product>({
@@ -23,34 +58,9 @@ const Trade: React.FC = () => {
 		setProduct({ ...product, id: location.state || 'AAPL' })
 	}, [])
 
-	// 最佳五檔資訊
-	const sellQuoteList = Array.from(Array(5).keys()).map(item => {
-		return (
-			<div className="flex justify-between" key={ item }>
-				<span> { getRandom(100) } </span>
-				<span> { product.price + (item + 1) } </span>
-			</div>
-		)
-	})
-	const buyQuoteList = Array.from(Array(5).keys()).map(item => {
-		return (
-			<div className="flex justify-between" key={ item }>
-				<span> { getRandom(100) } </span>
-				<span> { product.price - (item + 1) } </span>
-			</div>
-		)
-	})
-
 	// trade form
-	const [ tradeForm, setTradeForm ] = useState({
-		price: 0,
-		amount: 1,
-	})
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setTradeForm({ ...tradeForm, [event.target.name]: event.target.value })
-	}
-	
-	
+	const [ tradeForm, setTradeForm ] = useState({ forcePrice: 0, amount: 1 })
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setTradeForm({ ...tradeForm, [event.target.name]: event.target.value })
 
 	return (
 		<div className="flex flex-col">
@@ -67,26 +77,26 @@ const Trade: React.FC = () => {
 					<div className="m-auto">線圖</div>
 				</div>
 
-				<div className="text-center">
-					<h4 className="my-2">最佳五檔</h4>
-					<div className="mx-4 w-2/5 inline-flex flex-col text-green"> { buyQuoteList } </div>
-					<div className="mx-4 w-2/5 inline-flex flex-col text-red"> { sellQuoteList } </div>
-				</div>
+				<TradeQuote price={ product.price } />
 			</main>
 			
 			<footer className="p-2 text-xl text-white text-center">
-				<div className="my-4 flex justify-around">
+				<div className="my-4 flex justify-around items-center">
 					<span>價格</span>
-					<input name="price" className="w-4/5 bg-transparent border-b border-active" type="text" onChange={ handleChange }/>
+					<TradeButton sign="-" />
+					<input name="forcePrice" className="w-3/5 bg-transparent border-b border-active" type="text" onChange={ handleChange } />
+					<TradeButton sign="+" />
 				</div>
-				<div className="my-4 flex justify-around">
+				<div className="my-4 flex justify-around items-center">
 					<span>股數</span>
-					<input name="amount" className="w-4/5 bg-transparent border-b border-active" type="text"/>
+					<TradeButton sign="-" />
+					<input name="amount" className="w-3/5 bg-transparent border-b border-active" type="text" onChange={ handleChange } />
+					<TradeButton sign="+" />
 				</div>
 
 				<div className="flex">
-					<button className="m-2 p-2 w-1/2 text-center bg-green rounded-lg" >賣出</button>
-					<button className="m-2 p-2 w-1/2 text-center bg-red rounded-lg" >買入</button>
+					<button className="m-2 p-2 w-1/2 text-center bg-green rounded-lg">賣出</button>
+					<button className="m-2 p-2 w-1/2 text-center bg-red rounded-lg">買入</button>
 				</div>
 			</footer>
 		</div>
