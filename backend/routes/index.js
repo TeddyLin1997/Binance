1e1155e76e59799b2e9dde65c8cd467e97d5656b
ChrisLin <i100308326@gmail.com>
2021-05-06 01:03:36 +0800

    refactor:routers configs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const usersRouter = require('./users');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const setControllers = (app) => {
+  //index
+  app.use('/', indexPage());
 
-module.exports = router;
+  // service
+  app.use('/users', usersRouter);
+
+  // error
+  errorHandler(app)
+}
+
+const indexPage = () => {
+  const router = express.Router();
+
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+  return router
+}
+
+const errorHandler = (app) => {
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}
+
+module.exports = setControllers
