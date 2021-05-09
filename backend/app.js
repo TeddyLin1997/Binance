7d6cd1be95539c274133ccc052920e75e7d06aa5
ChrisLin <i100308326@gmail.com>
2021-05-10 01:06:49 +0800

    chore:project build

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,18 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const setRouters = require('./routes/index');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const setRouters = require('./routes/index')
 
-const app = express();
+const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 // frontend project
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 setRouters(app)
 
-module.exports = app;
+module.exports = app
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,28 +1,32 @@
-const createError = require('http-errors');
-const usersRouter = require('./users');
+const createError = require('http-errors')
+const path = require('path')
+const usersRouter = require('./users')
 
 const setRouters = (app) => {
-  
-  app.use('/users', usersRouter);
+  setupHandler(app)
 
-  errorHandler(app)
+  app.use('/users', usersRouter)
 }
 
-const errorHandler = (app) => {
+const setupHandler = (app) => {
+  // view engine setup
+  app.set('views', path.join(__dirname, '../views'))
+  app.set('view engine', 'pug')
+
   // error status
   app.use(function(req, res, next) {
-    next(createError(404));
-  });
+    next(createError(404))
+  })
 
   // error handler
   app.use(function(err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-    res.status(err.status || 500);
-    res.render('error');
-  });
+    res.status(err.status || 500)
+    res.render('error')
+  })
 }
 
 module.exports = setRouters
