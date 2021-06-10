dfa97a9b0f60c905f3eb3124696b69fc11bc56a4
ChrisLin <i100308326@gmail.com>
2021-06-11 00:27:58 +0800

    feat: error msg transition

--- frontend/src/components/form-error/index.tsx
+++ frontend/src/components/form-error/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { colors } from 'style/index'
 
@@ -12,9 +12,17 @@ const ErrorLabel = styled.div<{ isShow: boolean }>`
   font-size: 12px;
   color: ${ colors.red };
   opacity: ${ props => props.isShow ? 1 : 0 };
-  transition: opacity .4s;
+  transition: opacity .2s;
 `
 
-const FormError = ({ msg }: FormError) => <ErrorLabel isShow={ msg !== '' }>{ msg }</ErrorLabel>
+const FormError = ({ msg }: FormError) => {
+  const [label, setLabel] = useState('')
+
+  useEffect(() => {
+    if (msg) setLabel(msg)
+  }, [msg])
+
+  return <ErrorLabel isShow={ msg !== '' }>{ label }</ErrorLabel>
+}
 
 export default React.memo(FormError)
