9a5cab00c66f0d9c6893b3f1fe3c4a05e998aafa
ChrisLin <i100308326@gmail.com>
2021-06-12 18:33:12 +0800

    chore: path config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
-      'style': path.resolve(__dirname, '/src/assets/style'),
       'images': path.resolve(__dirname, '/src/assets/images'),
       'action': path.resolve(__dirname, '/src/store/slice')
     },
