e14d012d93a85dadd95e119576229cec7870ae0f
ChrisLin <i100308326@gmail.com>
2021-06-22 00:45:14 +0800

    chore: sql error reconnect

--- backend/common/db.js
+++ backend/common/db.js
@@ -1,10 +1,29 @@
 const mysql = require('mysql')
 
-const db = mysql.createConnection({
+const DB_CONFIGS = {
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'binance',
-})
+}
 
-module.exports = db
+const handleDisconnect = () => {
+  db = mysql.createConnection(DB_CONFIGS)
+
+  db.connect(err => {
+    console.log(`Database connecting ${err ? 'error' : 'success' }`)
+
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  db.on('error', (err) => {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') handleDisconnect()
+    else throw err;                                  
+  });
+}
+
+module.exports = handleDisconnect
